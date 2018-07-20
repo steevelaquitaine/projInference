cc8b473d58ffcea66a1ae24c9fc617b3e031835b
steeve <steeve@stanford.edu>
2018-07-20 18:01:10 +0200

    updating task

---
+++ task/main.m
@@ -0,0 +1,19 @@
+
+% runTask.m
+%
+%                   by: Steeve Laquitaine
+%                 date: 140109
+%              purpose: each line runs a block of several trials where displayed motion directions are drawn from a prior %                       distribution over direction at 3 different coherences.
+%                usage: run line by line
+%          description: the code loads a parameter file:
+%                       e.g., "120925_steeve_exp03_metho_param_dir6_var0030_mean195_coh012_024_062_1_t125_eye.mat"
+
+taskDotDir('params_prior040_coh006012024')
+taskDotDir('params_prior020_coh006012024')
+taskDotDir('params_prior010_coh006012024')
+taskDotDir('params_prior080_coh006012024')
+
+
+
+
+
